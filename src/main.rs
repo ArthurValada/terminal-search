ac05727acef78dd0eae2c551fd17e752ef946a4e
Arthur Valadares Campideli <arthur.c.valadares@gmail.com>
2024-02-03 14:58:27 -0300

    Correction of in-code documentation

--- src/main.rs
+++ src/main.rs
@@ -36,30 +36,30 @@ fn log_init(file_path: PathBuf) {
 
 
 /// This class was created with the aim of representing a search engine.
-/// It makes use of the macros [Serialize], [Deserialize] and [Parse] so that it can be serialized and deserialized
+/// It makes use of the macros [Serialize], [Deserialize] and [Parser] so that it can be serialized and deserialized
 /// by serde \[feature= serde_yaml] and passed as arguments on the command line. This object contains the
 /// minimum settings for the system to function properly, regarding the search engine URL.
-/// Attributes:
-/// - [name], to represent the name of the search engine;
-/// - [url_pattern], to store the search engine url pattern;
-/// - [pattern], to store the replacement pattern being used in the url;
-/// - [regex], the regex that will be searched within the search term and replaced by [replacement];
 #[derive(Serialize, Deserialize, Debug, Parser, Clone)]
 pub struct Engine {
+    /// Represent the name of the search engine
     name: String,
+
+    /// Store the search engine url pattern;
     url_pattern: String,
+
+    /// Store the replacement pattern being used in the url
     pattern: String,
+
+    /// The regex that will be searched within the search term and replaced by replacement
     regex: String,
     replacement: String,
 }
 
 
 /// Implementation of the struct [Engine].
-/// Methods:
-/// - [new], used to create a new engine according to the values passed as arguments;
-/// - [url], used to generate the url based on the data already existing in the [Engine] object and based on the term,
-/// [term] passed as argument;
 impl Engine {
+
+    /// Create a new engine according to the values passed as arguments;
     pub fn new(name: &str, url_pattern: &str, pattern: &str, regex: &str, replacement: &str) -> Engine {
         info!("Creating a new engine.");
         Engine {
@@ -71,6 +71,9 @@ impl Engine {
         }
     }
 
+
+    /// Generate the url based on the data already existing in the [Engine] object and based on the term passed
+    /// as argument;
     pub fn url(self, term: &str) -> Result<String, io::Error> {
         info!("Generating a URL.");
 
@@ -102,46 +105,30 @@ impl Engine {
 /// Class created with the objective of storing all the configurations that the program supports.
 /// The [Configuration] class has the macros [Serialize] and [Deserialize], so that it can be serialized and
 /// deserialized by serde \[feature=serde_yaml], in order to be written to and read from a .yaml file
-/// Attributes:
-/// - [file_path], stores the configuration file path;
-/// - [default_engine], stores the name of the default search engine, null by default and subject to change,
-/// according to user preferences;
-/// - [engines], stores all objects representing search engines - [Engine]
 #[derive(Serialize, Deserialize, Debug)]
 struct Configuration {
+
+    /// Stores the configuration file path;
     #[serde(skip_serializing)]
     #[serde(skip_deserializing)]
     file_path: PathBuf,
 
+    /// Stores the name of the default search engine, null by default and subject to change, according to user preferences
     default_engine: Option<String>,
+
+    /// Stores all objects representing search engines - [Engine]
     engines: Option<Vec<Engine>>,
 }
 
 
 /// Implementation of the Configuration struct.
 /// About the macro: In order to provide possibly useful features for what the project may become.
-/// Some functions, whose scope is very well defined, are currently not applicable. To this end, in order
+/// Some functions, whose scope is very well-defined, are currently not applicable. To this end, in order
 /// to indicate to the compiler that there are no problems with the existence of _dead_ code, this directive is used
-/// Methods:
-/// - [new], responsible for creating a new instance of a configuration object based on the values passed as
-/// arguments;
-/// - [from] responsible for loading the configuration object from the file path passed as an argument.
-/// If the file does not exist, it is created, if it exists but is empty, a new default configuration object is
-/// created, if the file exists and is not empty, an attempt is made to load its configuration.
-/// - [save], saves the object contents to a .yaml file;
-/// - [push], adds an engine to the list of configured search engines;
-/// - [update_path], updates the file path. UNUSED;
-/// - [remove_where_name], removes a search engine based on name;
-/// - [remove_at], removes a search engine based on its position;
-/// - [names], generates a list of the names of the configured search engines;
-/// - [patterns], generates a list of patterns configured for each search engine;
-/// - [url_patterns], generates a list of url patterns from all search engines;
-/// - [regexes], generates a list with the regex of each search engine;
-/// - [replacements], generates a list of [replacements] for each search engine;
-/// - [set_default], sets the default search engine based on name;
-/// - [where_name], returns the search engine based on the name passed as an argument;
 #[warn(dead_code)]
 impl Configuration {
+
+    /// Responsible for creating a new instance of a configuration object based on the values passed as arguments
     pub fn new(file_path: PathBuf, default_engine: Option<String>, engines: Option<Vec<Engine>>) -> Configuration {
         info!("Creating a new settings.");
         Configuration {
@@ -151,6 +138,10 @@ impl Configuration {
         }
     }
 
+
+    /// Responsible for loading the configuration object from the file path passed as an argument.
+    /// If the file does not exist, it is created, if it exists but is empty, a new default configuration object is
+    /// created, if the file exists and is not empty, an attempt is made to load its configuration.
     pub fn from(file_path: PathBuf) -> Result<Configuration, io::Error> {
         info!("Load settings from {:?}", file_path);
 
@@ -193,6 +184,8 @@ impl Configuration {
         };
     }
 
+
+    /// Saves the object contents to a .yaml file
     pub fn save(self) -> Result<(), io::Error> {
         info!("Trying to save to file {:?}", self.file_path);
         match File::create(self.file_path.clone()) {
@@ -223,6 +216,8 @@ impl Configuration {
         }
     }
 
+
+    /// Adds an engine to the list of configured search engines
     pub fn push(&mut self, engine: Engine) {
         self.engines = self.engines.clone().map_or(Some(vec![engine.clone()]), |mut vector| {
             vector.push(engine);
@@ -230,10 +225,14 @@ impl Configuration {
         });
     }
 
+
+    /// Updates the file path
     pub fn update_path(&mut self, new: PathBuf) {
         self.file_path = new;
     }
 
+
+    /// Removes a search engine based on name
     pub fn remove_where_name(&mut self, name: &str) -> Result<(), io::Error> {
         return if let Some(content) = &mut self.engines {
             content.retain(|element| element.name != name);
@@ -245,6 +244,7 @@ impl Configuration {
     }
 
 
+    /// Removes a search engine based on its position on [self.engines]
     pub fn remove_at(self, index: usize) -> Result<(), io::Error> {
         match self.engines {
             Some(mut content) => {
@@ -258,6 +258,8 @@ impl Configuration {
         }
     }
 
+
+    /// Generates a list of the names of the configured search engines
     pub fn names(&self) -> Vec<String> {
         match &self.engines {
             Some(content) => content.iter().map(|element| element.name.clone()).collect(),
@@ -265,6 +267,8 @@ impl Configuration {
         }
     }
 
+
+    /// Generates a list of patterns configured for each search engine
     pub fn patterns(self) -> Vec<String> {
         match self.engines {
             Some(content) => content.iter().map(|element| element.pattern.clone()).collect(),
@@ -272,6 +276,8 @@ impl Configuration {
         }
     }
 
+
+    /// Generates a list of url patterns from all search engines
     pub fn ulr_patterns(self) -> Vec<String> {
         match self.engines {
             Some(content) => content.iter().map(|element| element.url_pattern.clone()).collect(),
@@ -279,6 +285,8 @@ impl Configuration {
         }
     }
 
+
+    /// Generates a list with the regex of each search engine
     pub fn regexes(self) -> Vec<String> {
         match self.engines {
             Some(content) => content.iter().map(|element| element.regex.clone()).collect(),
@@ -286,6 +294,8 @@ impl Configuration {
         }
     }
 
+
+    /// Generates a list of replacement for each search engine
     pub fn replacements(self) -> Vec<String> {
         match self.engines {
             Some(content) => content.iter().map(|element| element.replacement.clone()).collect(),
@@ -293,6 +303,8 @@ impl Configuration {
         }
     }
 
+
+    /// Returns the default search engine
     pub fn default(&self) -> Option<Engine> {
         match &self.default_engine {
             Some(default) => {
@@ -306,6 +318,8 @@ impl Configuration {
         }
     }
 
+
+    /// Sets the default search engine based on name
     pub fn set_default(&mut self, name: String) -> Result<(), io::Error> {
         if self.names().contains(&name) {
             self.default_engine = Some(name);
@@ -315,6 +329,8 @@ impl Configuration {
         }
     }
 
+
+    /// Returns the search engine based on the name passed as an argument
     pub fn where_name(&self, name: String) -> Result<Engine, io::Error> {
         if let Some(engines) = &self.engines {
             for engine in engines {
@@ -331,48 +347,47 @@ impl Configuration {
 }
 
 
-/// Classe responsável por fazer o intermédio da linha de comando com o executável.
-/// Faz-se uso do [Parse], pertencente ao *Clap*, para gerar a implementação para a linha de comando.
-/// Os macros [command] são usados para adicionar informações à linha de comando, conforme o nome de cada um.
-/// Atributos:
-/// - [term], o termo de busca a ser utilizado, possivelmente nulo, neste caso, o texto selecionado sera usado;
-/// - [angine], argumento opcional. Se nenhuma for especificada usar-se-à a padrão;
-/// - [commands], comandos passíveis de execução;
+/// Class responsible for intermediating the command line with the executable.
+/// [Parser], belonging to *Clap*, is used to generate the implementation for the command line.
+/// command macros are used to add information to the command line, according to their name.
 #[derive(Parser)]
 #[command(author = "Arthur Valadares Campideli", version, about = "A simple test application in rust", long_about = "This application was created with the aim of adding a shortcut to the keyboard in order to search the selected text")]
 #[command(propagate_version = true)]
 struct Cli {
+
+    /// The search term to be used, possibly null, in this case the selected text will be used
     #[arg(help = "Specify the term to be searched for")]
     term: Option<String>,
 
+    /// Optional argument. If none is specified, the default will be used
     #[arg(long, short, help = "Specifies the search engine to be used")]
     engine: Option<String>,
 
+    /// Commands that can be executed
     #[command(subcommand)]
     commands: Option<Commands>,
 }
 
 
 /// Enum containing the subcommands that can be executed from the Cli.
-/// Values:
-/// - [Commands::List], lists the configured search engines;
-/// - [Commands::Default], shows the default search engine configured;
-/// - [Commands::SetDefault], defines the search engine, taking the name of the search engine as an argument;
-/// - [Commands::Add], adds a search engine based on the values requested by [Engine::new];
-/// - [Commands::Remove], removes a search engine based on name;
 #[derive(Subcommand)]
 enum Commands {
+
+    /// Lists the configured search engines
     #[clap(about = "List configured search engines")]
     List,
 
+    /// Shows the default search engine configured
     #[clap(about = "Show default engine")]
     Default,
 
+    /// Defines the search engine, taking the name of the search engine as an argument
     #[clap(about = "Sets the default search engine", )]
     SetDefault {
         name: String,
     },
 
+    /// Adds a search engine based on the values requested by [Engine::new]
     #[clap(about = "Add a search engine")]
     Add {
         name: String,
@@ -383,6 +398,7 @@ enum Commands {
 
     },
 
+    /// Removes a search engine based on name
     #[clap(about = "Remove a search engine based on name")]
     Remove {
         name: String,
